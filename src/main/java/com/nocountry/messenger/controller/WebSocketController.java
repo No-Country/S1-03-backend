5ba04044ef5a65b0654c0bf342627380a32692c4
Juan Caballero <57407836+Juan-ci@users.noreply.github.com>
2022-04-19 15:50:09 -0300

    @Cross origin
    trying to solve crossorigin

--- src/main/java/com/nocountry/messenger/controller/WebSocketController.java
+++ src/main/java/com/nocountry/messenger/controller/WebSocketController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin
 public class WebSocketController {
     
     @Autowired
